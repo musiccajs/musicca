c70cf63a2f21f9d60fc37f481df41ddb69b21683
Edqe <34704796+Edqe14@users.noreply.github.com>
2021-12-31 17:41:41 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2019
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    semi: 'off',
+    quotes: 'off',
+    indent: 'off',
+    'no-multi-spaces': 'error',
+    'consistent-return': 'off',
+    'import/extensions': 'off',
+    'no-trailing-spaces': 'error',
+    'import/no-unresolved': 'off',
+    'object-curly-spacing': 'error',
+    'class-methods-use-this': 'off',
+    'linebreak-style': ['error', 'unix'],
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/quotes': ['error', 'single'],
+    '@typescript-eslint/member-delimiter-style': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+# Project Template
+
+A simple and rich Typescript project template for your development needs.
+
+## Quick Start 🌟
+
+1. Create a new project with this template
+2. Clone the project
+3. Install dependencies using **`yarn install`**
+4. Happy coding!
+
+## Features 📚
+
+1. 🌀 Typescript ready
+2. 🎨 Linting using [Eslint](https://eslint.org/)
+3. 🐶 [Husky](https://typicode.github.io/husky/#/) to improve your commits
+4. 🧪 Test with [Jest](https://jestjs.io/)
+
+## License 📋
+
+MIT
