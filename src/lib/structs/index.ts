d0f4488010f4181eda2e02fb8747291e504a3c26
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-01-01 03:12:51 +0700

    feat: add queue and its manager, bunch of changes

--- src/lib/structs/index.ts
+++ src/lib/structs/index.ts
@@ -1,2 +1,5 @@
 export { default as BasePlugin } from './BasePlugin';
 export { default as Song } from './Song';
+export { default as Extractor } from './Extractor';
+export { default as Queue, QueueConstructor } from './Queue';
+export * from './managers';
